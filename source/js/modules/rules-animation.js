2202dc42b23c438c56fa831cc15bdf32151e926f
Olga Kopyova <imkopyova@gmail.com>
2020-08-07 11:05:36 +0300

    fix: fix lint errors

--- source/js/modules/rules-animation.js
+++ source/js/modules/rules-animation.js
@@ -1,11 +1,8 @@
 export default () => {
   let lastRulesItem = document.querySelector(`.rules__item:last-child`);
   let rulesLink = document.querySelector(`.rules__link`);
-  console.log(lastRulesItem);
-  console.log(rulesLink);
 
-  lastRulesItem.onanimationend = function(event) {
-    console.log("end");
+  lastRulesItem.onanimationend = function () {
     rulesLink.classList.add(`animate`);
   };
-}
+};
